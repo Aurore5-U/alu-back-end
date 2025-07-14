551fd252c5bae8483cb6b95475129891562193b0
Aurore5-U <a.umumararu@alustudent.com>
2025-07-14 00:34:43 -0700

    fourth time is a charm

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-''' Test request to parse API's
-'''
+"""
+Exports all TODO tasks of a given user to a CSV file.
+Format: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+"""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        api_endpoint = "https://jsonplaceholder.typicode.com"
-        user_id = sys.argv[1]
-        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
-        username = user_data.get('username')
-        todo_data = \
-            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
-            json()
-        with open("{}.csv".format(user_id), 'w') as csv_file:
-            for task in todo_data:
-                csv_file.write('"{}","{}","{}","{}"\n'.format(
-                    user_id,
-                    username,
-                    task.get('completed'),
-                    task.get('title')
-                )
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: ./1-export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    api_base = "https://jsonplaceholder.typicode.com"
+
+    # Fetch user info
+    user_res = requests.get("{}/users/{}".format(api_base, user_id))
+    if user_res.status_code != 200:
+        print("User not found.")
+        sys.exit(1)
+    username = user_res.json().get("username")
+
+    # Fetch user's todos
+    todos_res = requests.get("{}/users/{}/todos".format(api_base, user_id))
+    todos = todos_res.json()
+
+    # Write to CSV file named <user_id>.csv
+    with open("{}.csv".format(user_id), mode="w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([user_id, username, task.get("completed"), task.get("title")])
+
--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-''' Test request to parse API's
-'''
-import csv
+"""
+Exports all TODO tasks of a given user to a JSON file.
+Format: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ...] }
+"""
 import json
 import requests
 import sys
@@ -10,15 +11,29 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1].isdigit():
         api_endpoint = "https://jsonplaceholder.typicode.com"
         user_id = sys.argv[1]
-        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
-        username = user_data.get('username')
-        todo_data = \
-            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
-            json()
+
+        # Get user data
+        user_response = requests.get("{}/users/{}".format(api_endpoint, user_id))
+        if user_response.status_code != 200:
+            print("User not found.")
+            sys.exit(1)
+        username = user_response.json().get("username")
+
+        # Get TODOs
+        todo_response = requests.get("{}/users/{}/todos".format(api_endpoint, user_id))
+        todo_data = todo_response.json()
+
+        # Build the task list
+        tasks = []
+        for task in todo_data:
+            tasks.append({
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": username
+            })
+
+        # Dump to JSON file
+        data = {user_id: tasks}
         with open("{}.json".format(user_id), 'w') as json_file:
-            tasks = []
-            for task in todo_data:
-                tasks.append({'task': task['title'],
-                              'completed': task['completed'],
-                              'username': username})
-            data = {"{}".format(user_id): tasks}
+            json.dump(data, json_file)
+
